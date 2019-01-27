8d3343734c93a2124a337c76927e291f23911436
nathanestill <nathan.estill@students.olin.edu>
2019-01-27 17:00:23 -0500

    adding Optimization

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int x = 5;
+	int y = x + 1;
+    printf("%d\n",y);
     return 0;
 }
+
+/*
+Ex 1. 	
+subq	$16, %rsp
+movl	$5, -4(%rbp)
+seems to make the int x = 5;
+
+Ex 2.
+I think that the program ignores making x as it is not relevent later.
+
+Ex 3.
+Printing x makes the code to run it show back up.
+
+Ex 4.
+I think that it just computes the value of y and ignores x.
+I think optimization just ignores irrelevant code and adjusts relevent existing code.
