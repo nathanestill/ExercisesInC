2041a60c8c107056caa4aa49ed2265b50a386cf7
nathanestill <nathan.estill@students.olin.edu>
2019-01-27 12:23:06 -0500

    put 2 functions in, need to comment and add one more

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int getCardValue(char cardName[3]);
+void updateCount(int val);
 int main()
 {
-	char card_name[3];
+	char cardName[3];
 	int count = 0;
 	do {
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
+		puts("Enter the cardName: ");
+		scanf("%2s", cardName);
 		int val = 0;
-		switch(card_name[0]) {
-			case 'K':
-			case 'Q':
-			case 'J':
-				val = 10;
-				break;
-			case 'A':
-				val = 11;
-				break;
-			case 'X':
-				continue;
-			default:
-				val = atoi(card_name);
-				if ((val < 1) || (val > 10)) {
-					puts("I don't understand that value!");
-					continue;
-				}
+		val = getCardValue(cardName);
+		if(val == -1){
+			continue;
 		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
-		printf("Current count: %i\n", count);
-	} while (card_name[0] != 'X');
+		updateCount(val);
+	} while (cardName[0] != 'X');
 	return 0;
+}
+int getCardValue(char cardName[3]){
+	int val = 0;
+	switch(cardName[0]) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			val = -1;
+			break;
+		default:
+			val = atoi(cardName);
+			if ((val < 1) || (val > 10)) {
+				puts("I don't understand that value!");
+				val = -1;
+			}
+	}
+	return val;
+}
+void updateCount(int val){
+	static int count = 0;
+	if ((val > 2) && (val < 7)) {
+		count++;
+	} else if (val == 10) {
+		count--;
+	}
+	printf("Current count: %i\n", count);
 }
